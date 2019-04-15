515f38f16985e67c9a5e2e05db6194a2f5c989dc
Dante Gennero <41801525+SirAlfy@users.noreply.github.com>
2019-04-15 00:54:36 -0400

    Delete sent data
    Data that was sent is now deleted and some comments.

--- writeToDC.php
+++ writeToDC.php
@@ -1,34 +1,35 @@
-
 <?php
-$host = "10.142.0.24"; // This VMs internal IP Address.
-$port = 8002; // Outgoing port.
-$DC = "104.196.147.215"; // DataCenter
+$host = "10.142.0.24"; 	//VMs Internal IP
+$port = 8002;		//VMs Port
+$DC = "104.196.147.215";//DataCenter IP
+$DCport = 8001;		//DataCenter Port
 $db = new PDO("mysql:host=localhost;dbname=EdgeAuto", 'edgeauto', 'edgeauto19!');
 $result = $db->query("Select * from message");
 $outArray = array();
 $comma_sep = "";
+$LastMsg_ID=0;
+//Extract data from the DB and put it into a string to be sent.
 foreach($result as $row) {
+		$LastMsg_ID = $row['message_id'];
         $outArray[] = $row['arb_id'];
         $outArray[] = $row['message'];
         $outArray[] = $row['latitude'];
         $outArray[] = $row['longitude'];
         $outArray[] = $row['cantime'];
         $outArray[] = $row['session_id'];
+		$LastSess_ID = $row['session_id'];
 		$comma_sep .= implode(",",$outArray);
 		$comma_sep .= ";";
 		$outArray = array();
 } 
-
-//print_r ($comma_sep);
-
+//Create a socket and connect to it. Then connect to the port on the datacenter and write the data.
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP) or die("Could not create socket\n");
 socket_set_option($socket, SOL_SOCKET, SO_REUSEADDR, 1);
 socket_bind($socket, $host, $port) or die("Could not bind to socket\n");
-socket_connect($socket, $DC);
+socket_connect($socket, $DC, $DCport);
 socket_write($socket, $comma_sep);
-/*
-fwrite($socket, $comma_sep);
 
-fclose($socket);*/
+// Delete all of the data sent to the Datacenter
+$db->query("delete from message where message_id <= $LastMsg_ID");
 socket_close($socket);
 ?>
