194d0c1dedaf35f8e2949d5ebf2bd8bdc4e7edc9
Dante Gennero <41801525+SirAlfy@users.noreply.github.com>
2019-04-17 11:21:58 -0400

    Update VMInfo.php

--- VMInfo.php
+++ VMInfo.php
@@ -1,6 +1,7 @@
 <?php
-$host = ""; 		//This VMs Internal IP can be acquired by running "hostname -I"
-$port = 8001;						//This VMs Port
-$DC = "104.196.147.215";	//DataCenter IP
-$DCport = 8002;					//DataCenter Port
+$hostin = "";        //This VMs Internal IP
+$hostportin = 8001;             //This VMs Port to catch incoming data
+$hostportout = 8002;            //This VMs outport to write to the datacenter
+$DC = "34.74.170.183";          //DataCenter IP
+$DCport = 8001;                 //DataCenter Port
 ?>
